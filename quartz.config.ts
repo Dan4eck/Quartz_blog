0c71a5dfe1e4cb5e3d51e5b35fc16b14a517597d
Gennady87 <77787779+Gennady87@users.noreply.github.com>
2024-11-25 16:45:07 +0700

    Obsidian manual commit 2024-11-25 16:45:07

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "💻 ПродСовет \nCommunity",
+    pageTitle: "💻 ПродСовет \nCommunity blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
