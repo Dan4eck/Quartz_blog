2e9411f1fdc84aa69eafb912eb7c94c2f2b49e00
Gennady87 <77787779+Gennady87@users.noreply.github.com>
2024-12-03 01:02:20 +0700

    Obsidian manual commit 2024-12-03 01:02:20

--- quartz.config.ts
+++ quartz.config.ts
@@ -94,10 +94,23 @@ const config: QuartzConfig = {
       Plugin.SyntaxHighlighting({
         theme: {
           light: "github-light",
-          dark: "github-dark",
+          dark: {
+            name: "custom-dark",
+            settings: {
+              backgroundColor: "#4c566a", // nord3 - фон блока кода
+              textColor: "#88c0d0",       // nord8 - цвет текста
+            },
+          },
         },
-        keepBackground: false,
-      }),
+        keepBackground: true, // Сохранить фоновый цвет
+      }),      
+      // Plugin.SyntaxHighlighting({
+      //   theme: {
+      //     light: "github-light",
+      //     dark: "github-dark",
+      //   },
+      //   keepBackground: false,
+      // }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
